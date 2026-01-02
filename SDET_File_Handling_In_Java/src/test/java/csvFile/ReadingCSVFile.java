14ee9ae7dc4ee2b56fc0c06fbe9759fa75421d6d
vivek <vivek.sirsath1@gmail.com>
2026-01-02 23:59:14 +0530

    Reading CSV file added

--- SDET_File_Handling_In_Java/src/test/java/csvFile/ReadingCSVFile.java
+++ SDET_File_Handling_In_Java/src/test/java/csvFile/ReadingCSVFile.java
@@ -2,17 +2,21 @@ package csvFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class ReadingCSVFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		// Specify CSV file path
+		// 1st WAY :- Specify CSV file path
 		// String csvFile = System.getProperty("user.dir") + "\\data\\csvFileData.csv";
 
-		// Specify file path using File class
-		File csvFile = new File(System.getProperty("user.dir") + "\\data\\csvFileData.csv");
+		// 2nd WAY :- Specify file path using File class
+		// File csvFile = new File(System.getProperty("user.dir") + "\\data\\csvFileData.csv");
+		
+		// 3rd WAY :- Specify file path using FileReader Class
+		FileReader csvFile = new FileReader(".\\data\\csvFileData.csv"); // FileNotFoundException
 
 		// Create object of Scanner class
 		Scanner sc = new Scanner(csvFile); // FileNotFoundException
